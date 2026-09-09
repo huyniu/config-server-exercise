91d0755a7ee6abf29f4fb66a56c572e3f9ce8ef6
huyniu <dhuy271105@gmail.com>
2026-09-09 23:17:47 +0700

    b1

---
+++ src/test/java/hunre/dh13c3/dinhquochuy/securerestapi/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hunre.dh13c3.dinhquochuy.securerestapi.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
